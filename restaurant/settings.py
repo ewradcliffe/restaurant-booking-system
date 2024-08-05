2817de0e874aab21656aa3a912909edc5b70d3e9
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-05 10:13:15 +0000

    Ready code for Heroku deployment.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ven-afwl=ya3#gb5xhep9$+lguxt#l=(4160ckph)165ckta(0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ewradcliffe-restaurantb-uyo2qqiblkh.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ewradcliffe-restaurantb-uyo2qqiblkh.ws.codeinstitute-ide.net','.herokuapp.com'
+]
 
 
 # Application definition
