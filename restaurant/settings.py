666d3f55da8e43935ee875319954840e00a8bec0
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-27 15:00:58 +0000

    Install Cloudinary to serve image files.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -55,6 +56,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'reservations',
 ]
 
