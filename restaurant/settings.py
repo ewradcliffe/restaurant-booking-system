8235ed9c452e28c9761b618e4def040cb15380f2
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-28 15:28:13 +0000

    Add menu app. Link URL.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'cloudinary',
     'reservations',
+    'menu',
 ]
 
 SITE_ID = 1
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+
 #from reservations import views as reservations_views
 
 
@@ -24,4 +26,5 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('menu/', include('menu.urls'), name='menu'),
 ]
