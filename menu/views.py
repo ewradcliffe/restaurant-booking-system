abcfb86f3ea1bb23de81280bae679c925a693893
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-28 17:08:34 +0000

    Add code to display menu in menu.html.

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from .models import Menu
 #from .models import Menu
 
 # Create your views here.
@@ -9,7 +10,6 @@ class menu(generic.ListView):
     """
     Renders the menu to the page
     """
-
-    #queryset = Menu.objects.all()
-    #template_name = "menu.html"
+    queryset = Menu.objects.all()
+    template_name = "menu/menu.html"
     
