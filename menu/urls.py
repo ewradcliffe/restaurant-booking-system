b5b0df3379cbf18d321baacfe81b16c265f8c09b
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-30 11:43:42 +0000

    Rewrite Menu view. Refactor menu.html code.

--- menu/urls.py
+++ menu/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.menu.as_view(), name='menu'),
+    path('', views.menu, name='menu'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
 from .models import Menu
-#from .models import Menu
+
 
 # Create your views here.
 
-class menu(generic.ListView):
+def menu(request):
     """
     Renders the menu to the page
     """
-    queryset = Menu.objects.all()
-    template_name = "menu/menu.html"
+    queryset = Menu.objects.all().values()
+
+    """Create a list of all menu_entry_types with entries in th database"""
+    menu_heading = [d.get('menu_entry_type', None) for d in queryset]
+
+    """Convert to dictionary and back to list to remove duplicates."""
+    menu_heading = list(dict.fromkeys(menu_heading))
+
+    context = {
+        'menu_heading': menu_heading,
+        'menu_list': queryset
+    }
+
+    return render(
+        request, 
+        "menu/menu.html",
+        context
+    )   
+    
     
