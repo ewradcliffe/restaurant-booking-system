a7719c793f8b1be7fbf5183da9d91467b31ac694
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-08-30 12:39:03 +0000

    Add test to check menu can be viewed while logged in.

--- menu/test_views.py
+++ menu/test_views.py
@@ -15,6 +15,14 @@ class TestMenuViews(TestCase):
             email="test@test.com"
         )
         
+        """Create instance of user"""       
+        self.user = User.objects.create_user(
+            username="username",
+            password="password123!",
+            email="test@test.com"
+        )
+        self.user.save()
+
         """Add menu item"""
         self.post = Menu(menu_entry_type="Main", menu_entry_by=self.user,
                          menu_entry_name="test name", menu_entry_description="test description",
@@ -24,6 +32,17 @@ class TestMenuViews(TestCase):
 
     def test_render_menu_view(self):
         """Confirm menu item can be seen without logging in."""
+        response = self.client.get(reverse('menu'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"main", response.content)
+        self.assertIn(b"test name", response.content)
+        self.assertIn(b"test description", response.content)
+        self.assertIn(b"9.99", response.content)
+
+
+    def test_render_menu_view_login(self):
+        """Confirm menu item can be seen after logging in."""
+        self.client.login(username='username', password='password123!')
         response = self.client.get(reverse('menu'))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"main", response.content)
