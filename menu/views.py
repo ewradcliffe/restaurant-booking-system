37ccefcdf2f95eb4e14fdf8dc31256070e3f29eb
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-09-01 14:43:42 +0000

    Refactor menu view.

--- menu/views.py
+++ menu/views.py
@@ -9,12 +9,7 @@ def menu(request):
     Renders the menu to the page
     """
     queryset = Menu.objects.all().values()
-
-    """Create a list of all menu_entry_types with entries in th database"""
-    menu_heading = [d.get('menu_entry_type', None) for d in queryset]
-
-    """Convert to dictionary and back to list to remove duplicates."""
-    menu_heading = list(dict.fromkeys(menu_heading))
+    menu_heading = ('Starter', 'Main', 'Dessert', 'Drinks')
 
     context = {
         'menu_heading': menu_heading,
