36b16fd3f07fde17da0cf55fbf3995f20b54d472
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-09-09 13:11:48 +0000

    Remove old secret key from settings.

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -26,13 +26,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ven-afwl=ya3#gb5xhep9$+lguxt#l=(4160ckph)165ckta(0'
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
